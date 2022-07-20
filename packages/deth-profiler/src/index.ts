aee24055f6ea1435ea55c435199847cd4c0d4384
Leonid Logvinov <logvinov.leon@gmail.com>
2022-07-20 12:51:18 +0200

    Add support for diff reports

--- packages/deth-profiler/src/index.ts
+++ packages/deth-profiler/src/index.ts
@@ -1,7 +1,7 @@
 import * as core from '@actions/core'
 import * as github from '@actions/github'
-import { LAYOUT, ProfilingResults, readJson } from '@deth-profiler/common'
-import { diffByTestProfilingResults } from '@deth-profiler/profiler'
+import { LAYOUT, ProfilingResults, readJson, SourceCodes } from '@deth-profiler/common'
+import { diffByTestProfilingResults, generateHtmlReport } from '@deth-profiler/profiler'
 import * as artifact from '@superactions/artifact'
 import { createCommentOrUpdate } from '@superactions/comment'
 import path from 'path'
@@ -25,30 +25,44 @@ const artifactKeys = {
  */
 async function main(): Promise<void> {
   const results = await readJson<ProfilingResults>(path.join(projectPath, LAYOUT.RESULTS))
-  const artifacts = await readJson<ProfilingResults>(path.join(projectPath, LAYOUT.ARTIFACTS))
+  const artifacts = await readJson<SourceCodes>(path.join(projectPath, LAYOUT.ARTIFACTS))
 
   core.info('Uploading results')
   await artifactClient.uploadValue(artifactKeys.results, results)
   core.info('Uploading artifacts')
   await artifactClient.uploadValue(artifactKeys.artifacts, artifacts)
-  core.info('Uploading html report')
-  await artifactClient.uploadDirectory(artifactKeys.report, path.join(projectPath, LAYOUT.REPORT_FOLDER))
+
+  const viewReportHydrationData = { results: results.byTest, sourceCodes: artifacts }
+  let diffReportHydrationData
 
   if (context.eventName === 'pull_request') {
     core.info('Running on PR')
 
     const baseResults = await artifactClient.downloadValue<ProfilingResults>(artifactKeys.results)
     if (baseResults) {
-      core.info('Found base results. Creating diff report.')
+      core.info('Found base results')
 
       const diffResults = diffByTestProfilingResults(baseResults.byTest, results.byTest)
+      core.info('Uploading diff results')
       await artifactClient.uploadValue(artifactKeys.diff, diffResults)
+
+      diffReportHydrationData = { diff: diffResults, sourceCodes: artifacts }
     }
   }
 
-  const url = artifactClient.getPageUrl(artifactKeys.report)
-  const message = `[HTML Report](${url})`
+  core.info('Generating html report')
+  await generateHtmlReport(artifactKeys.report, viewReportHydrationData, diffReportHydrationData)
 
+  core.info('Uploading html report')
+  await artifactClient.uploadDirectory(artifactKeys.report, path.join(projectPath, LAYOUT.REPORT_FOLDER))
+
+  const url = artifactClient.getPageUrl(artifactKeys.report)
+  let message = `[Profiling Report](${url}/view.html)`
+  if (diffReportHydrationData) {
+    message += `\n[Diff Report](${url}/diff.html)`
+  }
+
+  core.info('Creating or updating GH comment')
   await createCommentOrUpdate({
     message,
     uniqueAppId: 'superactions/deth-profiler',

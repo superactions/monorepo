a41d28ef83c3a1c8be2f248c0b2da704fbf49cb0
Leonid Logvinov <logvinov.leon@gmail.com>
2022-07-29 14:37:33 +0200

    Remove logging

--- packages/deth-profiler/src/index.ts
+++ packages/deth-profiler/src/index.ts
@@ -47,8 +47,6 @@ async function main(): Promise<void> {
 
       const diffResults = diffByTestProfilingResults(baseResults.byTest, results.byTest)
       core.info('Uploading diff results')
-      // eslint-disable-next-line no-console
-      console.log(JSON.stringify(diffResults, null, 2))
       await artifactClient.uploadValue(artifactKeys.diff, diffResults)
       core.info(`${artifactClient.getArtifactUrl(artifactKeys.diff)}`)
 
